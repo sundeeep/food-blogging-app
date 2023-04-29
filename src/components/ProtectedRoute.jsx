c107868f36961f46cb5ea63716841d92193ed257
sandeepkdasari143 <sandeepkumar25820@gmail.com>
2023-04-30 03:23:47 +0530

    Protected Route Bug Resolved

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
 
-const ProtectedRoute = ({ children}) => {
-    const userToken = useSelector(state=> state.googleAuth.useDetails.accessToken)
-    if (userToken) {
-        return children;
+const ProtectedRoute = ({ children }) => {
+    const userDetails = useSelector((state) => state.googleAuth.userDetails);
+    console.log(userDetails.accessToken)
+    if (!userDetails.accessToken) {
+        return <Navigate to="/login" replace={true} />;
+    } else {
+        return children
     }
-    <Navigate to="/login"/>
 }
 
 export default ProtectedRoute
